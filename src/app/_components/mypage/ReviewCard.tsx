3468d2fc8fae344c940eb99249375c2eb2b360d9
Sehee Jang <seheejang94@gmail.com>
2024-10-11 21:39:56 +0900

    add: 마이페이지를 위한 컴포넌트, 페이지, 타입, 유틸 기본구조

---
+++ src/app/_components/mypage/ReviewCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//  개별 리뷰를 카드 형식으로 표시하는 컴포넌트
+const ReviewCard = () => {
+  return <div>ReviewCard</div>;
+};
+
+export default ReviewCard;
---
+++ src/app/_components/mypage/ReviewList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// 리뷰 목록을 표시하는 컴포넌트
+const ReviewList = () => {
+  return <div>ReviewList</div>;
+};
+
+export default ReviewList;

ab4ee8a5418216df744f08e9ab6fd8999966145c
Sehee Jang <seheejang94@gmail.com>
2024-10-11 22:30:00 +0900

    add: Header추가,  fix: 파일 디렉토리 수정

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const MyPage = () => {
+  return (
+    <div>
+      <h1>마이페이지</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/mypage/myreviews">내가 쓴 리뷰</Link>
+            <Link href="/">관심 리스트</Link>
+          </li>
+          {/* 다른 링크들 */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MyPage;
--- src/app/pages/mypage/myReviews.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyReviews = () => {
-  return <div>Mypage</div>;
-};
-
-export default MyReviews;

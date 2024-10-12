aa1bb5ed96a7c93588faf1797764fd047dee7c5b
lukby2457 <kjh7442455@gmail.com>
2024-10-12 22:24:32 +0900

    feat: 회원가입 기능 구현 완료

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import browserClient from "@/utils/supabase/client";
+import Link from "next/link";
 import React from "react";
 
 const Loginpage = () => {
-  return <div>Loginpage</div>;
+  const check = async() => {
+    const data = await browserClient.auth.getSession();
+    return data.data;
+  }
+
+  console.log(check());
+
+  return (
+    <>
+      <form action="">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>로그인</button>
+      </form>
+      <span>
+        <p>계정이 없으신가요?</p>
+        <Link href={"/signup"}>회원가입</Link>
+      </span>
+    </>
+  )
 };
 
 export default Loginpage;

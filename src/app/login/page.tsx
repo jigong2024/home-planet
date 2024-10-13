65f216f294f3d711c358cb8bff0b7d98eaee4485
lukby2457 <kjh7442455@gmail.com>
2024-10-13 18:06:50 +0900

    feat: 로그인 페이지 로직 구현 완료

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,63 @@
-"use client"
+"use client";
 
 import browserClient from "@/utils/supabase/client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Loginpage = () => {
-  const check = async() => {
-    const data = await browserClient.auth.getSession();
-    return data.data;
-  }
+  const [info, setInfo] = useState<{ email: string; password: string }>({
+    email: "",
+    password: ""
+  });
 
-  console.log(check());
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>, keyName: string) => {
+    const value = event.target.value;
+    const newInfo = { ...info, [keyName]: value };
+    setInfo(newInfo);
+  };
+
+  const handleAddInfo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signInUser();
+  };
+
+  const signInUser = async () => {
+    try {
+      const { error } = await browserClient.auth.signInWithPassword({
+        email: info.email,
+        password: info.password
+      });
+
+      if (error) {
+        alert("이메일/패스워드를 다시 확인해주세요!");
+        throw error;
+      } else {
+        window.location.href = window.origin
+      }
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
 
   return (
     <>
-      <form action="">
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleAddInfo}>
+        <input
+          type="email"   
+          onChange={(event) => {
+            handleChangeInput(event, "email");
+          }}
+          placeholder="Email"
+          className="text-black"
+        />
+        <input
+          type="password"
+          onChange={(event) => {
+            handleChangeInput(event, "password");
+          }}
+          placeholder="Password"
+          className="text-black"
+        />
         <button>로그인</button>
       </form>
       <span>
@@ -24,7 +65,7 @@ const Loginpage = () => {
         <Link href={"/signup"}>회원가입</Link>
       </span>
     </>
-  )
+  );
 };
 
 export default Loginpage;

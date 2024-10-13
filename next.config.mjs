f0316b2638234784e33b7c66885d8151be133e01
hadooni <hayoung7259@gmail.com>
2024-10-13 22:54:23 +0900

    update: supabase url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        pathname: "/storage/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
